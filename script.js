6ff479237abb0b3774fd0ae6177a9c8306df207c
robert morales <rmoralfit@gmail.com>
2024-04-10 15:47:28 -0400

    added bubbleSort function to swap for a new array when init button is pressed

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const n=10;
 const array=[];
 
-for(let i=0;i<n;i++){
+
+function init(){
+    for(let i=0;i<n;i++){
     array[i]=Math.random();
+    }
+    showBars();
 }
 
-for(let i=0;i<array.length;i++){
-    const bar=document.createElement("div");
-    bar.style.height=array[i]*100+"%";
-    bar.classList.add("bar");
-    container.appendChild(bar);
+function play(){
+    bubbleSort(array);
+    showBars();
+}
+
+function bubbleSort(array){
+    do{
+     var swapped=false;
+     for(let i=1;i<array.length;i++){
+        if(array[i-1]>array[i]){
+            swapped=true;
+            [array[i-1],array[i]]=[array[i],array[i-1]];
+        }
+    }
+}
+while(swapped);
+}
+
+function showBars(){
+    container.innerHTML="";
+    for(let i=0;i<array.length;i++){
+        const bar=document.createElement("div");
+        bar.style.height=array[i]*100+"%";
+        bar.classList.add("bar");
+        container.appendChild(bar);
+    }
 }
