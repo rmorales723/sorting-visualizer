e2c624550096d95ca3578a15683c4b733bb4815f
robert morales <rmoralfit@gmail.com>
2024-04-11 15:44:41 -0400

    finally fixed animation to play() function and added red background to swapped bars in swaps=[]

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const n=10;
 const array=[];
 
+init();
 
 function init(){
     for(let i=0;i<n;i++){
@@ -9,30 +10,50 @@ function init(){
     showBars();
 }
 
-function play(){
-    bubbleSort(array);
-    showBars();
+function play() {
+    const copy = [...array];
+    const swaps = bubbleSort(copy);
+    animate(swaps);
+}
+
+function animate(swaps){
+    if(swaps.length==0){
+        showBars();
+        return;
+    }
+const [i,j]=swaps.shift();
+    [array[i],array[j]]=[array[j],array[i]];
+    showBars([i,j]);
+    setTimeout(function(){
+        animate(swaps);
+    },50);
 }
 
 function bubbleSort(array){
+    const swaps=[];
     do{
      var swapped=false;
      for(let i=1;i<array.length;i++){
         if(array[i-1]>array[i]){
             swapped=true;
+            swaps.push([i-1,i]);
             [array[i-1],array[i]]=[array[i],array[i-1]];
         }
     }
-}
-while(swapped);
+}while(swapped);
+return swaps;
 }
 
-function showBars(){
+function showBars(indices){
     container.innerHTML="";
     for(let i=0;i<array.length;i++){
         const bar=document.createElement("div");
         bar.style.height=array[i]*100+"%";
         bar.classList.add("bar");
+
+        if(indices && indices.includes(i)){
+            bar.style.backgroundColor="red";
+        }
         container.appendChild(bar);
     }
 }
